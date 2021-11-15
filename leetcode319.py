52f25351cd7bc2c19d8dd5bf840b689f27aec592
songchao <songchao5@cmbc.com.cn>
2021-11-15 09:44:29 +0800

    319 数学

---
+++ leetcode319.py
@@ -0,0 +1,18 @@
+# 319. 灯泡开关
+# 初始时有 n 个灯泡处于关闭状态。第一轮，你将会打开所有灯泡。接下来的第二轮，你将会每两个灯泡关闭一个。
+
+# 第三轮，你每三个灯泡就切换一个灯泡的开关（即，打开变关闭，关闭变打开）。第 i 轮，你每 i 个灯泡就切换一个灯泡的开关。直到第 n 轮，你只需要切换最后一个灯泡的开关。
+
+# 找出并返回 n 轮后有多少个亮着的灯泡。
+
+
+
+# 来源：力扣（LeetCode）
+# 链接：https://leetcode-cn.com/problems/bulb-switcher
+# 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+class Solution:
+    def bulbSwitch(self, n: int) -> int:
+        return int(pow(n,0.5))
+
+sol  = Solution()
+print(sol.bulbSwitch(5))

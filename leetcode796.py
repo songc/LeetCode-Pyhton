973f5a142d1b3d377cf84cda17a7b279c85e2e17
songchao <songchao5@cmbc.com.cn>
2022-04-09 09:34:24 +0800

    796 遍历

---
+++ leetcode796.py
@@ -0,0 +1,27 @@
+# 796. 旋转字符串
+# 给定两个字符串, s 和 goal。如果在若干次旋转操作之后，s 能变成 goal ，那么返回 true 。
+
+# s 的 旋转操作 就是将 s 最左边的字符移动到最右边。 
+
+# 例如, 若 s = 'abcde'，在旋转一次之后结果就是'bcdea' 。
+
+# 来源：力扣（LeetCode）
+# 链接：https://leetcode-cn.com/problems/rotate-string
+# 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+
+
+class Solution:
+    def rotateString(self, s: str, goal: str) -> bool:
+        n=len(s)
+        if n!=len(goal):
+            return False
+        for _ in range(n):
+            s=s[1:]+s[:1]
+            if s==goal:
+                return True
+        return False
+
+sol = Solution()
+s = "abcde"
+goal = "abced"
+print(sol.rotateString(s,goal))

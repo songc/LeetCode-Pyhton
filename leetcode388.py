117970db8b0232e1e5929ebe533ae390dbf00a59
songchao <songchao5@cmbc.com.cn>
2022-04-20 09:53:35 +0800

    388 栈

---
+++ leetcode388.py
@@ -0,0 +1,58 @@
+# 388. 文件的最长绝对路径
+# 假设有一个同时存储文件和目录的文件系统。下图展示了文件系统的一个示例：
+
+
+
+# 这里将 dir 作为根目录中的唯一目录。dir 包含两个子目录 subdir1 和 subdir2 。subdir1 包含文件 file1.ext 和子目录 subsubdir1；subdir2 包含子目录 subsubdir2，该子目录下包含文件 file2.ext 。
+
+# 在文本格式中，如下所示(⟶表示制表符)：
+
+# dir
+# ⟶ subdir1
+# ⟶ ⟶ file1.ext
+# ⟶ ⟶ subsubdir1
+# ⟶ subdir2
+# ⟶ ⟶ subsubdir2
+# ⟶ ⟶ ⟶ file2.ext
+# 如果是代码表示，上面的文件系统可以写为 "dir\n\tsubdir1\n\t\tfile1.ext\n\t\tsubsubdir1\n\tsubdir2\n\t\tsubsubdir2\n\t\t\tfile2.ext" 。'\n' 和 '\t' 分别是换行符和制表符。
+
+# 文件系统中的每个文件和文件夹都有一个唯一的 绝对路径 ，即必须打开才能到达文件/目录所在位置的目录顺序，所有路径用 '/' 连接。上面例子中，指向 file2.ext 的 绝对路径 是 "dir/subdir2/subsubdir2/file2.ext" 。每个目录名由字母、数字和/或空格组成，每个文件名遵循 name.extension 的格式，其中 name 和 extension由字母、数字和/或空格组成。
+
+# 给定一个以上述格式表示文件系统的字符串 input ，返回文件系统中 指向 文件 的 最长绝对路径 的长度 。 如果系统中没有文件，返回 0。
+
+# 来源：力扣（LeetCode）
+# 链接：https://leetcode-cn.com/problems/longest-absolute-file-path
+# 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+
+
+class Solution:
+    def lengthLongestPath(self, input: str) -> int:
+        fileList = input.split('\n')
+        ans = 0
+        stack= []
+        for i in range(len(fileList)):
+            lev,filename=self.getLevAndName(fileList[i])
+            while len(stack)>lev:
+                stack.pop()
+            stack.append(filename)
+            if filename.find('.')!=-1:
+                ans = max(len("/".join(stack)),ans)
+        return ans
+
+
+    def getLevAndName(self, filename:str):
+        i=0
+        lev = 0
+        while i <len(filename):
+            if filename[i]=='\t':
+                lev+=1
+            else:
+                break
+            i+=1
+        return lev,filename[lev:]
+
+
+sol = Solution()
+# inp = "dir\n\tsubdir1\n\tsubdir2\n\t\tfile.ext"
+inp = "file1.txt\nfile2.txt\nlongfile.txt"
+print(sol.lengthLongestPath(inp))

5c7a909852e7cd08fd5a333d69ad8eaf04a3e6f3
songchao <songchao5@cmbc.com.cn>
2022-06-30 09:53:12 +0800

    324 ** 摆动排序

--- leetcode324.py
+++ leetcode324.py
@@ -1,19 +1,28 @@
-# 347. 前 K 个高频元素
-# 给定一个非空的整数数组，返回其中出现频率前 k 高的元素。
-import collections
+# 324. 摆动排序 II
+# 给你一个整数数组 nums，将它重新排列成 nums[0] < nums[1] > nums[2] < nums[3]... 的顺序。
+
+# 你可以假设所有输入数组都可以得到满足题目要求的结果。
+
+# 来源：力扣（LeetCode）
+# 链接：https://leetcode.cn/problems/wiggle-sort-ii
+# 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+
 from typing import List
+
+
 class Solution:
-    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        counter = collections.Counter(nums)
-        return [ i[0] for i in counter.most_common(k)]
-
-class Solution2:
-    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        counter = collections.Counter(nums)
-        kv = sorted(((k,counter[k]) for k in counter), key=lambda kv: -kv[1])
-        return [kv[i][0] for i in range(k)]
-
-sol = Solution2()
-nums = [1,1,1,2,2,3] 
-k = 2
-print(sol.topKFrequent(nums,k))
+    def wiggleSort(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        tmp = sorted(nums)
+        left, right = (len(tmp)-1)//2, len(nums)-1
+        ind = 0
+        while ind<len(nums):
+            nums[ind]=tmp[left]
+            ind+=1
+            if ind<len(nums):
+                nums[ind]=tmp[right]
+            left-=1
+            right-=1
+            ind+=1
